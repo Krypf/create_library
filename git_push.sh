5541dd0c532de334b9b64a19ee660c64b6bd5012
Krypf <krypf2@gmail.com>
2024-09-23 02:28:30 +0900

    This is 'git push' by git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,10 +1,13 @@
-COMMENT=$1
+#!/bin/sh
 
-if [ -z "$COMMENT" ]; then
-  echo "Usage: $0 <library_name>"
+ADD_TARGET=$1
+COMMENT=$2
+
+if [ -z "$ADD_TARGET" ] || [ -z "$COMMENT" ]; then
+  echo "Usage: $0 <add_target> <commit_message>"
   exit 1
 fi
 
-git add .
-git commit -m "changed $COMMENT"
+git add "$ADD_TARGET"
+git commit -m "$COMMENT"
 git push
